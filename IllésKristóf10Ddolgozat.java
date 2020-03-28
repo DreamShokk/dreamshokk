e1a37741164180f03b315d93739030001705bf31
DreamShokk <59744539+DreamShokk@users.noreply.github.com>
2020-03-28 12:34:45 +0100

    Update IllésKristóf10Ddolgozat.java

--- IllésKristóf10Ddolgozat.java
+++ IllésKristóf10Ddolgozat.java
@@ -1,4 +1,4 @@
-//készítette Illék Kristóf, csak referencia értékként kezelendő.
+//készítette Illés Kristóf, csak referencia értékként kezelendő.
 import java.io.*;
 import java.util.*;
 public class IllésKristóf10Ddolgozat {
